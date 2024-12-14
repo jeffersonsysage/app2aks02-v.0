0ecb90c48f678d8e2ac1bd7b4ada515e9d47d3a4
jeffersonsysage <jeffersondeng@metaage.com.tw>
2024-12-14 14:59:12 +0000

    record backend api success and dont know why

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,22 @@
+# from flask import Flask, jsonify
+# from flask_cors import CORS
+
+# app = Flask(__name__)
+# CORS(app)  # 這行會為所有路由啟用 CORS
+
+# @app.route('/api')
+# def hello_world():
+#     return jsonify({"msg": "Hello World"})
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # 這行會為所有路由啟用 CORS
+CORS(app)  # 这行会为所有路由启用 CORS
 
 @app.route('/api')
 def hello_world():
